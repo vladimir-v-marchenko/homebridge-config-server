ede9da590f2caeebace7015d6c00451825d7950b
Nagisa Yata <kanbai.8ta.n@gmail.com>
2016-12-08 16:50:50 +0900

    [WIP]tmp

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# apt-get
+sudo apt-get update
+sudo apt-get upgrade
+
+# Install node
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo update-alternatives --install /usr/bin/node node /usr/bin/nodejs 10
+
+# Install Avahi
+sudo apt-get install libavahi-compat-libdnssd-dev
+
+# Node modules global path
+NODE_PATH = `npm -g root`
+
+# Install dependencies
+sudo npm install -g --unsafe-perm homebridge hap-nodejs node-gyp
+cd $NODE_PATH/homebridge/
+sudo npm install --unsafe-perm bignum
+cd $NODE_PATH/hap-nodejs/node_modules/mdns
+sudo node-gyp BUILDTYPE=Release rebuild
+
+# Install config server and multi plugin 
+sudo npm install -g homebridge-config-server
+sudo npm install -g homebridge-multi
+cd $NODE_PATH/homebridge-config-server/
+npm install
